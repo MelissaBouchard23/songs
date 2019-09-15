ebff9bfb836979daed12c26db2ee2228baf4b547
Mélissa Bouchard <pandora25_@hotmail.com>
2019-09-15 15:55:16 -0400

    Updates

---
+++ precache-manifest.5f1bb1cf1d73547c2eded4247267a586.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031ac4ce4745dbd31c8893d231ef00e7",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "3fdd6dcad5e5f5a0f0af",
+    "url": "/songs/static/js/2.9a1547ca.chunk.js"
+  },
+  {
+    "revision": "dad76526f0f42fc43df3",
+    "url": "/songs/static/js/main.e4968fb1.chunk.js"
+  },
+  {
+    "revision": "77f4c99e3f14b0bfcaae",
+    "url": "/songs/static/js/runtime~main.97be8660.js"
+  }
+]);
